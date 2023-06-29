a48a235c32f3edb2a38b47db74888aed3eb8db43
PhuocDuy <nguyenphuocduy2908@gmail.com>
2023-06-29 20:29:50 +0700

    duy

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,10 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('DDNC', 'root', 'Thanhls0123', {
-  host: 'localhost',
-  port: '3306',
-  dialect: 'mysql'  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+const sequelize = new Sequelize("DDNC", "root", null, {
+  host: "localhost",
+  port: "3306",
+  dialect:
+    "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
 });
 
-module.exports = sequelize
+module.exports = sequelize;
